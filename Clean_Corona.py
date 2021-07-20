d24ed179591dab24c836863fe2e1dd260408da48
owr7 <45285076+owr7@users.noreply.github.com>
2021-07-20 13:03:06 +0300

    Update Clean_Corona.py

--- Clean_Corona.py
+++ Clean_Corona.py
@@ -36,7 +36,6 @@ def count_recovering(model):
 class PoliceAgent(Agent):
     def __init__(self, unique_id: int, model: Model):
         super().__init__(unique_id, model)
-        # self.initial_position = 0
         self.health = HealthStatus.HEALTHY
 
     def move(self):
@@ -81,12 +80,10 @@ class PeopleAgent(Agent):
     def contagious(self, cellmates):
         if self.health == HealthStatus.ILL:
             for p in cellmates:
-                # if p.health == HealthStatus.HEALTHY and type(p) == PeopleAgent:
                 if np.random.random() < mask_protection(self.mask, p.mask,
                                                         protection_level=self.model.influence_rate) * \
                         (1 + p.health * self.recovery_immunity):
                     p.health = HealthStatus.ILL
-                    # p.time_infection = self.model.time
 
     def update_influence(self, cellmates):
         wearing_mask = [c for c in cellmates if c.mask]
@@ -103,7 +100,6 @@ class PeopleAgent(Agent):
                                    min(policeman, 1), self.social_influence,
                                    crowded / 10,
                                    ])
-        # print('mask:', threshold)
         if np.random.random() < threshold:
             self.mask = True
         else:
@@ -123,7 +119,6 @@ class PeopleAgent(Agent):
             self.health = HealthStatus.TEISH
 
     def travel_agent(self):
-        # return True
         if self.active:
             if np.random.random() < self.model.agent_turnover_rate:
                 self.active = False
@@ -214,20 +209,6 @@ class CoronaModel(Model):
         self.datacollector_6 = DataCollector(
             model_reporters={"Policy": self.get_current_policy},
             agent_reporters={"Health": "health"})
-
-        # EXPERT VERSION
-        # if len(kwargs) > 0:
-        #
-        #     self.disease_importing = kwargs['disease_importing']
-        #     self.number_of_hospital_beds = kwargs['disease_importing']
-        #     self.cop_area = width / kwargs['number_of_cops']
-
-        # SIMPLE VERSION
-        # else:
-        #     self.agent_turnover_rate = -1
-        #     self.disease_importing = -1
-        #     self.number_of_hospital_beds = -1
-        #     self.cop_max_dist = -1
 
         for i in range(self.num_agents):
             a = PeopleAgent(i, self, HealthStatus.HEALTHY)
